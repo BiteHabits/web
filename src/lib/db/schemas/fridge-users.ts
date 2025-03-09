82743d09e02520ff39b396984d29046979e44d02
andreashenriksen <andreassoland.99@gmail.com>
2025-03-09 15:43:23 +0100

    Fixed formatting

--- src/lib/db/schemas/fridge-users.ts
+++ src/lib/db/schemas/fridge-users.ts
@@ -4,25 +4,27 @@ import { users } from './users';
 import { fridges } from './fridges';
 
 export const fridgeUsers = sqliteTable(
-    'fridge_user',
-    {
-        user_id: text().notNull().references(() => users.id, { onDelete: 'cascade' }),
-        fridge_id: text().notNull().references(() => fridges.id, { onDelete: 'cascade' }),
-    },
-    (t) => [
-        primaryKey({ columns: [t.user_id, t.fridge_id]})
-    ]
+	'fridge_user',
+	{
+		user_id: text()
+			.notNull()
+			.references(() => users.id, { onDelete: 'cascade' }),
+		fridge_id: text()
+			.notNull()
+			.references(() => fridges.id, { onDelete: 'cascade' })
+	},
+	(t) => [primaryKey({ columns: [t.user_id, t.fridge_id] })]
 );
 
 export const fridgeUsersRelations = relations(fridgeUsers, ({ one }) => ({
-    user: one(users, {
-        fields: [fridgeUsers.user_id],
-        references: [users.id]
-    }),
-    fridge: one(fridges, {
-        fields: [fridgeUsers.fridge_id],
-        references: [fridges.id]
-    })
+	user: one(users, {
+		fields: [fridgeUsers.user_id],
+		references: [users.id]
+	}),
+	fridge: one(fridges, {
+		fields: [fridgeUsers.fridge_id],
+		references: [fridges.id]
+	})
 }));
 
 export type fridgeUsers = InferSelectModel<typeof fridgeUsers>;
