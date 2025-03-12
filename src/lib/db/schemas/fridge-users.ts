d9ecbd6955ce2af9ec3fa57fba1597a8a111aaec
Andreas S. Henriksen <andreassoland.99@gmail.com>
2025-03-12 15:25:37 +0100

    BH-17: Add user-fridge connection table. (#4)
    * BH-17: Add user-fridge connection table.
    
    * Removed comma in fridgeRelations
    
    * Fixed formatting
    
    * NO-ISSUE: Renamed class fridgeUsers -> FridgeUser
    
    * NO-ISSUE: generated sql file

---
+++ src/lib/db/schemas/fridge-users.ts
@@ -0,0 +1,30 @@
+import { relations, type InferSelectModel } from 'drizzle-orm';
+import { sqliteTable, text, primaryKey } from 'drizzle-orm/sqlite-core';
+import { users } from './users';
+import { fridges } from './fridges';
+
+export const fridgeUsers = sqliteTable(
+	'fridge_user',
+	{
+		user_id: text()
+			.notNull()
+			.references(() => users.id, { onDelete: 'cascade' }),
+		fridge_id: text()
+			.notNull()
+			.references(() => fridges.id, { onDelete: 'cascade' })
+	},
+	(t) => [primaryKey({ columns: [t.user_id, t.fridge_id] })]
+);
+
+export const fridgeUsersRelations = relations(fridgeUsers, ({ one }) => ({
+	user: one(users, {
+		fields: [fridgeUsers.user_id],
+		references: [users.id]
+	}),
+	fridge: one(fridges, {
+		fields: [fridgeUsers.fridge_id],
+		references: [fridges.id]
+	})
+}));
+
+export type FridgeUser = InferSelectModel<typeof fridgeUsers>;
--- src/lib/db/schemas/index.ts
+++ src/lib/db/schemas/index.ts
@@ -2,4 +2,5 @@ export * from './passwords';
 export * from './sessions';
 export * from './users';
 export * from './fridges';
+export * from './fridge-users';
 export * from './products';
