ab1d0f72539d2d959ad8dd9118eab9dbb86c6f7e
toretveita <toretveita@gmail.com>
2025-03-12 13:06:55 +0100

    NO-ISSUE: Migration conflicts

--- drizzle/0004_bent_tusk.sql
+++ drizzle/0004_bent_tusk.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `fridge` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL
+	`fridge_id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`user_id` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `product` (
