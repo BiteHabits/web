4f177c1305269ca36ddc293f296a023977a7a049
KaroGil <karolinkagil@gmail.com>
2025-03-17 08:25:16 +0100

    fix bug

--- src/lib/services/product.service.ts
+++ src/lib/services/product.service.ts
@@ -1,9 +1,10 @@
 import { db } from '$lib/db/drizzle';
 import { isPast } from 'date-fns';
 import { eq } from 'drizzle-orm';
+import { products } from '$lib/db/schemas';
 
 export const getProduct = async (productId: string) => {
-	const product = await db.query.product.findFirst({
+	const product = await db.query.products.findFirst({
 		where: (row, { eq }) => eq(row.id, productId),
 		with: {
 			fridgeId: true
@@ -15,7 +16,7 @@ export const getProduct = async (productId: string) => {
 	}
 
 	if (product.expiresAt && isPast(product.expiresAt)) {
-		await db.delete(product).where(eq(product.id, product.id));
+		await db.delete(products).where(eq(products.id, productId));
 		return null;
 	}
 
@@ -29,4 +30,33 @@ export const getProduct = async (productId: string) => {
 		fridgeId,
 		product: productWithoutFridgeId
 	};
+};
+
+export const getProductsFromFridge = async (fridgeId: string) => {
+	const productsFromFridge = await db.query.products.findMany({
+		where: (row, { eq }) => eq(row.fridgeId, fridgeId),
+		with: {
+			fridgeId: true
+		}
+	});
+
+	const filteredProducts = productsFromFridge
+		.filter((product) => {
+			if (product.expiresAt && isPast(product.expiresAt)) {
+				db.delete(products).where(eq(products.id, product.id));
+				return false;
+			}
+
+			if (product.quantity <= 0) {
+				return false;
+			}
+
+			return true;
+		})
+		.map(({ fridgeId, ...product }) => ({
+			fridgeId,
+			product
+		}));
+
+	return filteredProducts;
 };
