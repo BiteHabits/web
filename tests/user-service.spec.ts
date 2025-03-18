2e150fafe867539c8b8e4ec7546c3cea5d597b6d
andreashenriksen <andreassoland.99@gmail.com>
2025-03-18 19:24:10 +0100

    BH-23: add tests for user service

---
+++ tests/user-service.spec.ts
@@ -0,0 +1,63 @@
+import { givenUser } from './fixtures/users';
+import { describe, expect, it } from 'vitest';
+import { getUserById, createUser, updateUser, deleteUser } from '$lib/services/user.service';
+
+describe('UserService', () => {
+	it('should return a user', async () => {
+		await givenUser({ id: 'user-1', name: 'user1', email: 'user1@email.com' });
+
+		const user = await getUserById('user-1');
+
+		expect(user).not.toBe(null);
+		expect(user?.id).toBe('user-1');
+		expect(user?.name).toBe('user1');
+		expect(user?.email).toBe('user1@email.com');
+	});
+
+	it('should create a user', async () => {
+		const createdUser = await createUser({
+			id: '0',
+			name: 'user1',
+			email: 'user1@email.com'
+		});
+
+		expect(createdUser).not.toBeNull();
+		expect(createdUser?.name).toBe('user1');
+		expect(createdUser?.email).toBe('user1@email.com');
+
+		const foundUser = await getUserById(createdUser?.id);
+
+		expect(foundUser).not.toBeNull();
+		expect(foundUser?.name).toBe('user1');
+		expect(foundUser?.email).toBe('user1@email.com');
+	});
+
+	it('should update a user', async () => {
+		await givenUser({ id: 'user-1', name: 'user1', email: 'user1@email.com' });
+
+		const updatedUser = await updateUser('user-1', {
+			name: 'newuser1',
+			email: 'newuser1@email.com'
+		});
+
+		expect(updatedUser).not.toBeNull();
+
+		const foundUser = await getUserById('user-1');
+
+		expect(foundUser).not.toBeNull();
+		expect(foundUser?.name).toBe('newuser1');
+		expect(foundUser?.email).toBe('newuser1@email.com');
+	});
+
+	it('should delete a user', async () => {
+		await givenUser({ id: 'user-1', name: 'user1', email: 'user1@email.com' });
+
+		const deletedUser = await deleteUser('user-1');
+
+		expect(deletedUser).not.toBeNull();
+
+		const userAfterDeletion = await getUserById('user-1');
+
+		expect(userAfterDeletion).toBeNull();
+	});
+});
