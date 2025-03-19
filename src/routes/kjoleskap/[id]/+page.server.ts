4005bab1aad96881ed08a7be15b92337f2a9cc4c
Ole Magnus Fon Johnsen <me@omfj.no>
2025-03-19 11:31:28 +0100

    BH-34: Register products in fridge page

--- src/routes/kjoleskap/[id]/+page.server.ts
+++ src/routes/kjoleskap/[id]/+page.server.ts
@@ -1,7 +1,8 @@
 import { db } from '$lib/db/drizzle';
 import { eq } from 'drizzle-orm';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
+import { products } from '$lib/db/schemas';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const fridgeId = params.id;
@@ -22,4 +23,47 @@ export const load: PageServerLoad = async ({ params }) => {
 		fridge,
 		products
 	};
+};
+
+export const actions: Actions = {
+	default: async ({ request, locals, params }) => {
+		const user = locals.auth?.user;
+		if (!user) {
+			return {
+				success: false,
+				error: 'You must be logged in to add a product.'
+			};
+		}
+
+		const formData = await request.formData();
+		const name = formData.get('name') as string | null;
+		const expiryDate = formData.get('expiry_date') as string | null;
+		const quantity = formData.get('quantity') as string | null;
+
+		if (!name || !expiryDate || !quantity) {
+			return {
+				success: false,
+				error: 'Invalid input'
+			};
+		}
+
+		const quantityNumber = parseInt(quantity, 10);
+		if (isNaN(quantityNumber)) {
+			return {
+				success: false,
+				error: 'Quantity must be a number'
+			};
+		}
+
+		await db.insert(products).values({
+			name,
+			expiresAt: expiryDate,
+			quantity: quantityNumber,
+			fridgeId: params.id
+		});
+
+		return {
+			success: true
+		};
+	}
 };
--- src/routes/kjoleskap/[id]/+page.svelte
+++ src/routes/kjoleskap/[id]/+page.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+
 	let { data } = $props();
 	let { fridge, products } = $derived(data);
 </script>
 
 <h1>Products in {fridge.name}</h1>
+
+<h1>Register Product</h1>
+
+<form class="mx-auto flex max-w-md flex-col gap-4" method="post" use:enhance>
+	<label class="flex flex-col">
+		Name
+		<input name="name" class="rounded border p-1" required />
+	</label>
+
+	<label class="flex flex-col">
+		Expiry Date
+		<input type="date" name="expiry_date" class="rounded border p-1" required />
+	</label>
+
+	<label class="flex flex-col">
+		Quantity
+		<input type="number" name="quantity" class="rounded border p-1" required />
+	</label>
+
+	<button class="rounded bg-blue-500 p-1">Register Product</button>
+</form>
+
+<hr />
 
 <ul>
 	{#each products as product}
