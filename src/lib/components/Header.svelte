740e4efeec85719e2dde62b51dac85c3c18da415
KaroGil <karolinkagil@gmail.com>
2025-03-25 14:01:36 +0100

    Add recipes page with styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,37 +1,50 @@
 <script lang="ts">
 	import Logo from '$lib/assets/bite-habits-logo-trans.png';
+	import UserIcon from '$lib/assets/user-icon.svg';
 	import { getUser } from '$lib/context/user-context';
 
 	let user = getUser();
 </script>
 
-<header class="flex items-center justify-between p-4">
-	<div>
-		<img src={Logo} class="h-8 w-8" alt="BiteHabits logo" />
+<header class="flex items-end justify-between h-32 p-4 bg-green-300 mb-10">
+	<div class="flex items-center gap-1 flex-col">
+		<a href="/"><img src={Logo} class="h-16 w-16" alt="BiteHabits logo" /></a>
+		<h1 class="font-bold">BiteHabits</h1>
 	</div>
 
-	<menu class="flex items-center gap-4">
-		<li>
-			<a class="text-gray-700 hover:text-gray-900" href="/">Hjem</a>
-		</li>
-
-		{#if !!$user}
-			<li>
-				<a class="text-gray-700 hover:text-gray-900" href="/fridges">Kjøleskap</a>
+	<menu class="flex items-center gap-12 flex-row">
+		{#if $user}
+			<li class="p-4 border-2 border-gray-700 rounded-lg hover:bg-green-500">
+				<a class="p-4 text-gray-700 hover:text-gray-900" href="/recipes">Oppskrifter</a>
 			</li>
 
+			<li class="p-4 border-2 border-gray-700 rounded-lg hover:bg-green-500">
+				<a class="p-4 text-gray-700 hover:text-gray-900" href="/add-product">Legg til varer</a>
+			</li>
+
+			<li class="p-4 border-2 border-gray-700 rounded-lg hover:bg-green-500">
+				<a class="p-4 text-gray-700 hover:text-gray-900" href="/fridges">Kjøleskap</a>
+			</li>
+		{/if}
+	</menu>
+	{#if $user}
+		<menu class="flex items-center flex-col">
+			<li>
+				<img src={UserIcon} alt="user icon" />
+			</li>
 			<li>
 				<button class="text-gray-700 hover:text-gray-900">Logg ut</button>
 			</li>
-		{/if}
-
-		{#if !$user}
+		</menu>
+	{/if}
+	{#if !$user}
+		<menu>
 			<li>
 				<a class="text-gray-700 hover:text-gray-900" href="/logg-inn">Logg inn</a>
 			</li>
 			<li>
 				<a class="text-gray-700 hover:text-gray-900" href="/registrer">Registrer</a>
 			</li>
-		{/if}
-	</menu>
+		</menu>
+	{/if}
 </header>
---
+++ src/routes/recipes/[id]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let data;
+	const recipe = data.recipe;
+</script>
+
+<main class="relative flex justify-center">
+	<section class="absolute left-6 top-6 flex flex-col bg-yellow-100 rounded-xl shadow-md w-72 overflow-hidden">
+		<h2 class="font-semibold text-lg px-4 py-3 border-b-4 border-yellow-200">Ingredienser</h2>
+		<ul class="divide-y divide-gray-300">
+			{#each recipe.ingredients as ingredient}
+				<li class="px-4 py-2">{ingredient.name}</li>
+			{/each}
+		</ul>
+	</section>
+
+	<section class="flex flex-col items-center gap-6 max-w-2xl text-center py-8">
+		<img src={recipe.image} alt="recipe" class="w-96 h-96 object-cover rounded-lg border" />
+		
+		<h1 class="text-2xl font-bold">{recipe.name}</h1>
+
+		<section class="flex flex-col gap-4 items-center">
+			<h2 class="font-semibold text-lg">Slik gjør du det:</h2>
+			<ol class="list-decimal text-left list-inside space-y-2 px-4">
+				{#each recipe.steps as step}
+					<li>{step}</li>
+				{/each}
+			</ol>
+		</section>
+	</section>
+</main>
+
