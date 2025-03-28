48d6df54d3e49c12b25615801638d6a61f92a16d
KaroGil <karolinkagil@gmail.com>
2025-03-28 12:43:34 +0100

    add frontpage tekst

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
 <h1 class="py-8 text-center text-3xl font-bold">BiteHabits</h1>
+<p class="mx-auto flex w-1/2 justify-center text-center text-lg">
+	BiteHabits er en web applikasjon som hjelper deg med å holde oversikt over dine spisevaner og
+	kosthold. Du kan holde styr på hva du har i kjøleskapet, oppskrifter og måltider, og få forslag
+	til sunne alternativer basert på hva du har tilgjengelig, i håp om å redusere matsvinn.
+	<br />
+	<br />
+	BiteHabits er utviklet med SvelteKit og Tailwind CSS, og er en del av et prosjekt for å lære mer om
+	moderne webutvikling.
+	<br />
+	<br />
+	Applikasjonen er fortsatt under utvikling, og det vil komme flere funksjoner og forbedringer i fremtiden.
+	<br />
+	<br />
+</p>
