f64d6079387b13b0760db93e0773922b49fdb1e3
Ole Magnus Fon Johnsen <me@omfj.no>
2025-04-18 23:43:28 +0200

    NO-ISSUE: Use free tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,11 @@ resource "azurerm_service_plan" "plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+
+  # free tier configuration
+  sku_name                     = "F1"
+  maximum_elastic_worker_count = 1
+  per_site_scaling_enabled     = false
 }
 
 resource "azurerm_linux_web_app" "app" {
@@ -25,15 +29,33 @@ resource "azurerm_linux_web_app" "app" {
 
   https_only = true
 
+  logs {
+    application_logs {
+      file_system_level = "Information"
+    }
+
+    http_logs {
+      file_system {
+        retention_in_days = 2
+        retention_in_mb   = 35
+      }
+    }
+
+    detailed_error_messages = true
+    failed_request_tracing  = true
+  }
+
   site_config {
+    always_on = false
+
     application_stack {
-      docker_image_name        = "${var.docker_owner}/${var.docker_image}:${var.docker_tag}"
-      docker_registry_url      = "https://ghcr.io"
+      docker_image_name   = "${var.docker_owner}/${var.docker_image}:${var.docker_tag}"
+      docker_registry_url = "https://ghcr.io"
     }
   }
 
   app_settings = {
-    DATABASE_URL = var.database_url
+    DATABASE_URL        = var.database_url
     DATABASE_AUTH_TOKEN = var.database_auth_token
   }
 }
